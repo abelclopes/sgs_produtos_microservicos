df4c6213fed6b83e1fa90d6191c16385149b23df
Abel Lopes <abellopes@gmail.com>
2024-08-16 20:25:55 -0300

    swagger api gateway

--- src/ProductAuthGateway/Program.cs
+++ src/ProductAuthGateway/Program.cs
@@ -1,22 +1,29 @@
-using MMLib.SwaggerForOcelot.DependencyInjection;
-using MMLib.SwaggerForOcelot.Middleware;
+using Swashbuckle.AspNetCore.SwaggerGen;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
+using MMLib.SwaggerForOcelot.DependencyInjection;
+using MMLib.SwaggerForOcelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddControllers();
 builder.Services.AddOcelot();
 builder.Services.AddSwaggerForOcelot(builder.Configuration);
 
-builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+builder.Configuration.SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
 
 var app = builder.Build();
 
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
 app.UseSwaggerForOcelotUI(options =>
 {
-    options.PathToSwaggerGenerator = "/swagger/docs"; // Geração de Swagger
-});
-
-await app.UseOcelot();
+    options.PathToSwaggerGenerator = "/swagger/docs";
+}).UseOcelot().Wait();
 
 app.Run();
